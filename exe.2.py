2497adaae2059ce297361c3cbfe511171e242930
GEOVANNA DE ALMEIDA GARCIA <106485117+GeovannaAlmeidaGarcia@users.noreply.github.com>
2025-03-18 16:14:23 -0300

    Add files via upload

---
+++ exe.2.py
@@ -0,0 +1,96 @@
+''' 1
+nome = str ("João")
+print(nome)'''
+import math
+
+'''2
+a = int ('5')
+b = int ("10")
+s = a + b
+sub = a - b
+mul = a * b
+div = a / b
+print(f"A soma é {s}, subtração é {sub}, multiplicação é {mul} e a divisão é {div}")'''
+
+'''3
+preco = float ("50")
+desconto = float ("10")
+desc = preco/desconto
+pf = preco - desc
+print(f"O valor do desconto é: {pf}")'''
+
+'''4
+resultado = 10 + 5 * 2
+print(f"O valor é {resultado}")'''
+
+'''5
+texto = "150"
+tx = int (texto)
+print(f"{tx}")'''
+
+'''7
+a = int(input("Digite um valor: "))
+b = int(input("Digite outro valor: "))
+r = a + b
+print("Essa é a soma dos valores: " , r)'''
+
+'''8
+x = int(input("Escreva um numero: "))
+y = int(input("Escreva um numero: "))
+r = x//y
+print("O resultado é", r)'''
+
+'''9
+num1 = int(input("Escreva um numero: "))
+num2 = int(input("Escreva um numero: "))
+r = num1> num2
+print("A variavel num1 é maior que num2? ", r)'''
+
+'''10
+idade = int(input("Escreva um numero: "))
+r = idade*365
+print(f"Você viveu {r} dias")'''
+
+'''11
+base = int(input("Escreva a base: "))
+expoente = int(input("Escreva o expoente: "))
+r = (base) ** expoente
+print("O valor é: ", r)'''
+
+'''12
+preco = int(input("Valor: "))
+string = str(preco)
+print("O preço é R$: ",string)'''
+
+'''13
+raio = float(input("Valor do raio: "))
+a = 3.14 * raio**2
+print("O valor da área é :{:.2f}".format(a))'''
+
+'''14
+a = int(input("Escreva um numero: "))
+b = int(input("Escreva outro: "))
+print(a,b)
+a, b = b,a
+print(a,b)'''
+
+'''15
+nota1 = int(input("Escreva nota 1: "))
+nota2 = int(input("Escreva nota 2: "))
+nota3 = int(input("Escreva nota 3: "))
+r = (nota1 + 2+nota2 + 3+nota3 + 5) / 3+2+5
+print("A média ponderada é:{:.2f}".format(r))'''
+
+'''16
+import math
+x1 = int(input("Escreva x1: "))
+y1 = int(input("Escreva y1: "))
+x2 = int(input("Escreva x2: "))
+y2 = int(input("Escreva y2: "))
+
+r = math.sqrt((x2 - x1)**2 +(y2 - y1)**2)
+print("O valor é: ", r)'''
+
+
+
+
